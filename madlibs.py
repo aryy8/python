13c164c7fa0abff9054370e967f6cf336eafa7f7
aryy8 <aryanfeb17@gmail.com>
2024-12-31 16:31:39 +0530

    Initial commit

---
+++ madlibs.py
@@ -0,0 +1,6 @@
+name=input("enter your name:")
+game=input("enter game's name:")
+learning=input("enter what do you want to learn:")
+madlibs=f"hi!! I am {name} doing my first small project using python.\
+this game is called {game} in which blanks are filled using fstrings. this is a good project to start my {learning} journey."
+print(madlibs)
---
+++ number_guesser.py
@@ -0,0 +1,34 @@
+import random
+def guess(x):
+    rand_num=random.randint(1,x)
+    guess=0
+    while guess!=rand_num:
+        guess=int(input(f"guess a number bw 1 and {x}:"))
+        if guess<rand_num:
+            print("guessed no is too low!")
+        elif guess>rand_num:
+            print("guessed no is too high!")    
+        else:
+            print("yayyyy you guessed it correct!!!") 
+guess(10)               
+
+def comp_guess(x):
+    low=1
+    high=x
+    feedback=""
+    while feedback !="c":
+        if low!=high:
+         guess=random.randint(low,high)
+        else:
+         guess=low 
+        feedback=input(f"enter if {guess} is too high(h), too low(l), or correct(c)")        
+        if feedback == "h":
+          high = guess - 1
+        elif feedback == "l":
+         low = guess + 1
+        elif feedback == "c":
+         print(f"Yay! I guessed your number, {guess}, correctly!")
+        else:
+         print("Invalid input. Please enter 'H', 'L', or 'C'.")
+    print(f"yayyy comp guessed {guess} rightttt!!!!!")     
+comp_guess(20)       
